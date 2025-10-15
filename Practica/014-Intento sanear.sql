3ee2cd1b39daebe5ca0283685b6f1c25984a1fd7
Dani <danieloliveiravidal12@gmail.com>
2025-10-15 14:05:30 +0200

    Create 014-Intento sanear.sql

---
+++ Practica/014-Intento sanear.sql
@@ -0,0 +1,14 @@
+ALTER TABLE clientes
+	ADD CONSTRAINT comprobar_email
+	CHECK (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$');
+	
+-- Eliminamos registros que no cumplen
+
+DELETE FROM clientes WHERE identificador = 3;
+
+SELECT * FROM clientes;
+
+-- Ahora sí, aplicamos
+ALTER TABLE clientes
+	ADD CONSTRAINT comprobar_email
+	CHECK (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$');
