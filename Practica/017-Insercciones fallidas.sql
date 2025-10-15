39f02f6078927f6fc7a1f916fddeccf56a7b1f30
Dani <danieloliveiravidal12@gmail.com>
2025-10-15 14:08:55 +0200

    Update 017-Insercciones fallidas.sql

--- Practica/017-Insercciones fallidas.sql
+++ Practica/017-Insercciones fallidas.sql
@@ -16,20 +16,4 @@ INSERT INTO clientes VALUES(
 	'algo'
 );
 
-ERROR 3819 (HY000): Check constraint 'comprobar_email' is violated.
-
-INSERT INTO clientes VALUES(
-	NULL,
-	'12345678Z',
-	'Daniel',
-	'Oliveira Vidal',
-	'info@elorange.com'
-);
-
-SELECT * FROM clientes;
-+---------------+-----------+--------+----------------+-------------------------------+
-| identificador | dni       | nombre | apellidos      | email                         |
-+---------------+-----------+--------+----------------+-------------------------------+
-|             4 | 26639136F | Daniel | Oliveira Vidal | danieloliveiravidal@gmail.com |
-+---------------+-----------+--------+----------------+-------------------------------+
-1 row in set (0,00 sec)
+ERROR 3819 (HY000): Check constraint 'comprobar_email' is violated
