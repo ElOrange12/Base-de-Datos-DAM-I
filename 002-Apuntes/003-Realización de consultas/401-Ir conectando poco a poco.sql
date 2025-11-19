8d800e34513bcb95095fc23f1c985aee5def2e30
Dani <danieloliveiravidal12@gmail.com>
2025-11-19 12:55:01 +0100

    19/11/2025

---
+++ 002-Apuntes/003-Realización de consultas/401-Ir conectando poco a poco.sql
@@ -0,0 +1,38 @@
+-- sudo mysql -u root -p
+
+USE clientes;
+
+SELECT * FROM clientes;
+
++-----------+-----------+------+
+| nombre    | apellidos | edad |
++-----------+-----------+------+
+| Paula     | Fernandez |   28 |
+| Patricia  | Vidal     |   39 |
+| Pau       | Serrano   |   22 |
+| Pilar     | Dominguez |   51 |
+| Paco      | Alvarez   |   47 |
+| Petra     | Navarro   |   33 |
+| Pascual   | Hernandez |   54 |
+| Puri      | Crespo    |   41 |
+| Pablo     | Romero    |   36 |
+| Pol       | Castillo  |   27 |
+| Penelope  | Muñoz     |   30 |
+| Prospero  | Rey       |   63 |
+| Paolo     | Rivas     |   29 |
+| Priscila  | Marin     |   26 |
+| Piedad    | Cano      |   48 |
+| Piero     | Benitez   |   35 |
+| Petronila | Carmona   |   52 |
+| Pamela    | Ortiz     |   34 |
+| Plácido   | Rubio     |   56 |
+| Pablo     | Molina    |   23 |
+| Perla     | Suarez    |   31 |
+| Prince    | Garcia    |   40 |
+| Patricio  | Torres    |   37 |
+| Portia    | Lozano    |   28 |
+| Percy     | Vega      |   44 |
+| Pina      | Herrero   |   33 |
+| Pius      | Calderón  |   50 |
++-----------+-----------+------+
+
