ed08bd7e22adb6ac179c25f24e142d7abd06ddf0
Dani <danieloliveiravidal12@gmail.com>
2025-12-10 14:16:16 +0100

    10/12/2025

--- 002-Apuntes/008-Proyectos/002-CRUD/101-Ejercicios/003-Update/002-Procesa.php
+++ 002-Apuntes/008-Proyectos/002-CRUD/101-Ejercicios/003-Update/002-Procesa.php
@@ -21,14 +21,15 @@
   while ($fila = $resultado->fetch_assoc()) {
   	// 3.-Pintar un formulario en pantalla
     echo '
-    	<form action="003-procesaractualizacion.php" method="POST">
-      	<input type="text" name="nombre" placeholder="nombre" value="'.$fila['nombre'].'">
-        <input type="text" name="puesto" placeholder="puesto" value="'.$fila['puesto'].'">
-        <input type="text" name="salario" placeholder="salario" value="'.$fila['salario'].'">
-        <input type="text" name="fecha_contratacion" placeholder="fecha_contratacion" value="'.$fila['fecha_contratacion'].'">
-        <input type="text" name="departamento" placeholder="departamento" value="'.$fila['departamento'].'">
-        <input type="submit">
-      </form>
+		<form action="003-procesaractualizacion.php" method="POST">
+			<input type="hidden" name="id" value="'.$id.'">
+		  	<input type="text" name="nombre" placeholder="nombre" value="'.$fila['nombre'].'">
+			<input type="text" name="puesto" placeholder="puesto" value="'.$fila['puesto'].'">
+			<input type="text" name="salario" placeholder="salario" value="'.$fila['salario'].'">
+			<input type="text" name="fecha_contratacion" placeholder="fecha_contratacion" value="'.$fila['fecha_contratacion'].'">
+			<input type="text" name="departamento" placeholder="departamento" value="'.$fila['departamento'].'">
+			<input type="submit">
+		</form>
     ';
     // 4.-Enviar la información modificada a un tercer archivo
   }
